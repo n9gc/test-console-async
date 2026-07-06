3112e5daab129293e3eb680dc080d3e181d890f7
E0SelmY4V <yabo1887415157@qq.com>
2026-07-07 04:04:22 +0800

    add(test-console-async): 初始化仓库

---
+++ packages/test-console-async/lib/index.ts
@@ -0,0 +1,17 @@
+/**
+ * 测试用的 TS 包
+ * @license MIT
+ * @author accurtype
+ */
+declare module './index.ts';
+
+/**
+ * 得到一个问候
+ * @param n 要问候斐波那契数列的第几项
+ * @returns 问候
+ */
+export default function hello(n: number): string {
+	const helloString = `hello, ${n}!`;
+	console.log(helloString);
+	return helloString;
+}
