407550c94df31ec9f14ed6d9ea18bd2ae07ca9c5
E0SelmY4V <yabo1887415157@qq.com>
2026-07-07 03:16:19 +0800

    env: 初始化仓库
    去除 rust 内容，管理项目信息

--- packages/test-ts/lib/index.ts
+++ packages/test-ts/lib/index.ts
@@ -5,15 +5,13 @@
  */
 declare module '.';
 
-import { fibonacci } from '@accurtype/mono-test-wasm';
-
 /**
  * 得到一个问候
  * @param n 要问候斐波那契数列的第几项
  * @returns 问候
  */
 export default function hello(n: number): string {
-	const helloString = `hello, ${fibonacci(n)}!`;
+	const helloString = `hello, ${n}!`;
 	console.log(helloString);
 	return helloString;
 }
